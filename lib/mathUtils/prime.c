11e1c30162c2e63c11a89dc8585bfea61f761aec
beetlejade <riveiroilian@gmail.com>
2019-12-27 12:37:12 +0100

    fix bugs

--- lib/mathUtils/prime.c
+++ lib/mathUtils/prime.c
@@ -1,14 +1,13 @@
 #include "mathUtils.h"
 
 int prime(int a){
-	if(a<=1) { return FALSE; }
-	if(a<=3) { return TRUE; }
-	if(n%2==0 || n%3 ==0 ) { return FALSE; }
+	if(a<=1) { return 0; }
+	if(a<=3) { return 1; }
+	if(a%2==0 || a%3 ==0 ) { return 0; }
 	
 	int i = 5;
-	while(i*i<=n) {
-		if(n%i==0 || n%(i+2)==0) { return FALSE; }
-		i = i +6;
-	}
-	return TRUE;
-
+	while(i*i<=a) {
+		if(a%i==0 || a%(i+2)==0) { return 0; }
+		i+=6;
+	} return 1;
+}
